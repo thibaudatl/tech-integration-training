631abe00283f512b429560b59f7b01214b7aa559
leo thibaudat <leo.thibaudat.16@eigsi.fr>
2020-01-08 15:31:17 -0500

    initial login API

---
+++ code-correction/ProductImport/ProductImport.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$clientBuilder = new \Akeneo\Pim\ApiClient\AkeneoPimClientBuilder('http://localhost:8088/');
+$client = $clientBuilder->buildAuthenticatedByPassword(
+    '2_28q0eh5zrvgggscgkgs4kwk8gcsc4sw8wgwg8c0sks8gggw0s4',
+    '4yyftxudoesc4wk88oc4sowkw0k4gs4c4oco444cs0ksckkw0c',
+    'admin',
+    'admin'
+);
+
+var_dump($client->getToken());
+
+/*
+ * Credentials for the API
+ *
+ * Client ID: 2_3xe8n56l3dq8ckggsw88sc8ggo08kcg8swc8w0ogwkokg4w40c
+ * Secret: 6ngcps70r9oock8ss408g880ksssskgco0kc0w0k4cw8ccg0w
+ * Username: productimport_2_3800
+ * Password: 614e3555f
+ *
+*/
+
+$product = $client->getProductApi()->get('Tshirt-divided-blue-s');
